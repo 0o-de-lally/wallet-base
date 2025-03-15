19d6a44238064add7269c24924dc045eda2abbfb
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2025-03-15 16:25:42 -0400

    use beta release openlibra sdk

---
+++ e2e/__tests__/metaDocker.ts
@@ -0,0 +1,29 @@
+import { noop } from '../../util/noop';
+import { LOCAL_TESTNET_API, testnetDown, testnetUp } from 'open-libra-sdk/dist/index.cjs';
+import axios from 'axios';
+
+beforeEach(async () => {
+  console.log("testnet setup");
+  console.log(LOCAL_TESTNET_API);
+  // make sure we teardown any zombies first
+  await testnetDown();
+  await testnetUp();
+}, 60 * 1000); // 60 second timeout
+
+afterEach(async () => {
+  console.log("testnet teardown");
+  await testnetDown();
+}, 60 * 1000); // 60 second timeout
+
+describe('noop', () => {
+  it('should return true', () => {
+    expect(noop()).toBe(true)
+  })
+})
+
+describe('LOCAL_TESTNET_API', () => {
+  it('should respond to a GET request', async () => {
+    const response = await axios.get(LOCAL_TESTNET_API);
+    expect(response.status).toBe(200);
+  });
+});
