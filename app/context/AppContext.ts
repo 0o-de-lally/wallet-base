3834b8a76a79f729c11fc55e8a6c17293c2a1892
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2025-03-16 14:57:56 -0400

    fix

--- app/context/AppContext.ts
+++ app/context/AppContext.ts
@@ -1,15 +1,23 @@
-import { NetworkConfigGenerator } from '@/util/networkSettings';
-import { ChainName, NetworkConfig } from '../../types/networkTypes';
 import { createContext, useContext } from 'react';
+import { ChainName, NetworkConfig } from '@/types/networkTypes';
 
-export interface AppContextType {
+interface AppContextType {
   chain_name: ChainName;
   network_config: NetworkConfig;
 }
 
-export const AppContext = createContext<AppContextType>({
+const AppContext = createContext<AppContextType>({
   chain_name: ChainName.MAINNET,
-  network_config: NetworkConfigGenerator.generateConfig(ChainName.MAINNET)
+  network_config: {} as NetworkConfig
 });
 
-export const useAppContext = () => useContext(AppContext);
+export const useAppContext = () => {
+  const context = useContext(AppContext);
+  if (context === undefined) {
+    throw new Error('useAppContext must be used within an AppContext.Provider');
+  }
+  return context;
+};
+
+export { AppContext };
+export default AppContext;
