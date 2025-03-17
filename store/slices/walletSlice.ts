48c46d19abcbf14e6de0912137825aa841bc0150
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2025-03-17 16:23:34 -0400

    patch

--- store/slices/walletSlice.ts
+++ store/slices/walletSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import type { LibraWallet, Network } from 'open-libra-sdk';
+import { LibraWallet, Network } from 'open-libra-sdk';
 
 interface WalletState {
   walletConfig: {
