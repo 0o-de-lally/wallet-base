0c756667365b3f7d9c3529025ac087dddad3f39f
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2025-08-19 14:55:12 -0400

    remove pin

--- components/settings/Settings.tsx
+++ components/settings/Settings.tsx
@@ -49,7 +49,6 @@ export const Settings: React.FC<SettingsProps> = observer(
             | `/profiles`
             | `/create-account`
             | `/recover-account`
-            | `/pin`,
         );
       },
       [router],
